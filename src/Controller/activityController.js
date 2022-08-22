37c5eb6cfa295bf455fdb9cb38defe40930b178d
Alongkorn Thepwong <oyasumi.7w7@gmail.com>
2022-08-22 16:19:50 +0700

    update Route Controller singup login session

--- src/Controller/activityController.js
+++ src/Controller/activityController.js
@@ -2,9 +2,9 @@ const activities = require("../Models/activityModel");
 // const findByType = require('../Models/activityModel')
 
 const getAllActivities = async (req, res, next) => {
-  const { u_id } = req.query;
+  const { user_id } = req.query;
   const allActivity = await activities.find({
-    user_id: u_id
+    user_id: user_id
   });
   res.send(allActivity)
 }
@@ -22,16 +22,18 @@ const getActivityById = async (req, res, next) => {
 
 const createActivity = async (req, res, next) => {
   try {
-    const newActivity = new activities({
-      // activity_id: uuidv4(),
-      user_id: '62f32c1d78af39f80fa8aadd',
-      img: 001,
-      title: 'Teest',
-      type: 'Walking',
-      date: '2022-10-08',
-      time: '17:30',
-      description: 'testqqqqq'
-    });
+    const newActivity = new activities(req.body);
+    //   {
+    //   // activity_id: uuidv4(),
+    //   user_id: '62f32c1d78af39f80fa8aadd',
+    //   img: 001,
+    //   title: 'Teest',
+    //   type: 'Walking',
+    //   date: '2022-10-08',
+    //   time: '17:30',
+    //   description: 'testqqqqq'
+    // }
+   
     await newActivity.save();
     res.send(newActivity);
   } catch (error) {
