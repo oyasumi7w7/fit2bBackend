d3644cc610ac18a048b5b8734351f1d99e96bab1
Alongkorn Thepwong <oyasumi.7w7@gmail.com>
2022-08-24 16:36:47 +0700

    asd

--- index.js
+++ index.js
@@ -10,18 +10,3 @@ app.listen(PORT, () => {
     console.log("Express server listening on port " + PORT);
   });
   
-  
-// /users
-// const userRoutes = require("./routes/userRoute");
-// app.use("/users", userRoutes);
-
-// const PORT = process.env.PORT || 3000;
-  // const start = async = () => {
-//     await mongoose.connect('mongodb+srv://fit2b_admin:admin@fit2b.e1zgczo.mongodb.net/?retryWrites=true&w=majority');
-
-//     app.listen(port, () => {
-//         console.log(`Example app listening on port ${port}`)
-//     })
-// }
-
-// start();
